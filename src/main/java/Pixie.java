4e1da797b2adfde64f41a5b36ca972bb064d3fbc
Clement Ng <Clement.ng.62@outlook.com>
2026-01-27 12:18:50 +0800

    level 2: improve command handling; adds support for 'list' command and the adding of task

--- src/main/java/Pixie.java
+++ src/main/java/Pixie.java
@@ -1,19 +1,29 @@
 import java.util.Scanner;
 
 public class Pixie {
+
     public static void main(String[] args) {
+        TaskManager tasks = new TaskManager();
+        Scanner stdin = new Scanner(System.in);
+
         System.out.println("Hello! I'm Pixie");
         System.out.println("What can I do for you?");
 
-        Scanner stdin = new Scanner(System.in);
-        System.out.print("\n>>> ");
-        String line = stdin.nextLine();
-
-        while (!line.equals("bye")) {
-            System.out.println(line);
+        while (true) {
             System.out.print("\n>>> ");
-            line = stdin.nextLine();
+
+            String text = stdin.nextLine().strip();
+            String command = text.toLowerCase();
+
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else if (command.equals("list")) {
+                tasks.printTasks();
+            } else {
+                System.out.printf("added: %s ", text);
+                tasks.addTask(text);
+            }
         }
-        System.out.println("Bye. Hope to see you again soon!");
     }
 }
