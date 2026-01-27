56db89708324eb9bea6707a7441b3f6541d12094
Clement Ng <Clement.ng.62@outlook.com>
2026-01-27 11:51:23 +0800

    Level 1: Accepts user input from stdin and echo back commands sent

--- src/main/java/Pixie.java
+++ src/main/java/Pixie.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Pixie {
     public static void main(String[] args) {
-        System.out.println(" Hello! I'm Pixie");
-        System.out.println(" What can I do for you?");
-        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("Hello! I'm Pixie");
+        System.out.println("What can I do for you?");
+
+        Scanner stdin = new Scanner(System.in);
+        System.out.print("\n>>> ");
+        String line = stdin.nextLine();
+
+        while (!line.equals("bye")) {
+            System.out.println(line);
+            System.out.print("\n>>> ");
+            line = stdin.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
